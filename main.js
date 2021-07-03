94f9f686e6ae2b6731581ed761a65a4d143c3caa
Sebastián Martinez <sebamar88@gmail.com>
2021-07-03 20:29:48 -0300

    fixing

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const homeLink = document.querySelector('#home')
 const featuresLink = document.querySelector('#features')
 const pricingLink = document.querySelector('#pricing')
 const Logo = document.querySelector('.navbar-brand')
-const LoginDiv = document.querySelector('#login')
 
 //TODO: Descomentar estas variables cuando se elimine el TODO de linea 51
+//const LoginDiv = document.querySelector('#login')
 //const LoginForm = document.querySelector('#loginForm')
 //const nameLogin = document.querySelector('#nameLogin') 
 
@@ -47,33 +47,38 @@ pricingLink.addEventListener('click', (e)=>{
     userName = nameLogin.value;
     console.log(userName)
     LoginDiv.innerHTML = `<span>Bienvenido ${userName}</span><form class="d-inline-flex ms-5" id="logOut"><button class="btn btn-primary">LogOut</button></form>`
+
+    let logOutButton;
+
+    if(userName.trim() > 0){
+        logOutButton = document.querySelector('#logOut')
+        logOutButton.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            LoginDiv.innerHTML = ''
+            userName = '';
+            LoginDiv.innerHTML = `<form id="loginForm" action="">
+                                    <input type="text" name="name" id="nameLogin">
+                                    <input type="password" name="password" id="passLogin">
+                                    <input type="submit" class="btn btn-primary" value="Login">
+                                </form>`
+        })
+        
+    }
 }) */
 document.addEventListener('DOMContentLoaded', ()=>{
-//TODO: Eliminar hasta la linea 59 inclusive
+//TODO: Eliminar hasta la linea 85 inclusive
 
 let pedirNombre = prompt('Ingresa tu nombre completo:')
 
 while(pedirNombre === '' || pedirNombre === null){
     pedirNombre = prompt('Por favor ingrese un valor valido de nombre completo')
 }
-if(pedirNombre){
-    LoginDiv.innerHTML = `<span>Bienvenido ${pedirNombre}</span><form class="d-inline-flex ms-5" id="logOut"><button class="btn btn-primary">LogOut</button></form>`
+
+const Saludo = (nombre) => {
+    alert('Bienvenido ' + nombre + ', Cuanto tiempo sin verte!')
 }
 
-let logOutButton;
-
-if(userName.trim() > 0){
-    logOutButton = document.querySelector('#logOut')
-    logOutButton.addEventListener('submit', (e)=>{
-        e.preventDefault();
-        LoginDiv.innerHTML = ''
-        userName = '';
-        LoginDiv.innerHTML = `<form id="loginForm" action="">
-                                <input type="text" name="name" id="nameLogin">
-                                <input type="password" name="password" id="passLogin">
-                                <input type="submit" class="btn btn-primary" value="Login">
-                            </form>`
-    })
-    
+if(pedirNombre){
+    Saludo(pedirNombre)
 }
 })
